d9d40bda015abe229ba1117aa90b19cafce51c0e
PrathapGit98 <1998prathapchan@gmail.com>
2022-05-05 15:52:09 +0530

    m1

---
+++ src/org/company/CompanyDetails.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class CompanyDetails {
+	private void companyname() {
+		System.out.println("TCS");
+	}
+
+	private void CompanyId() {
+		System.out.println("2");
+	}
+
+	private void CompanyAddress() {
+		System.out.println("chrompet");
+	}
+
+	public static void main(String[] args) {
+
+		CompanyDetails CD = new CompanyDetails();
+		CD.companyname();
+		CD.CompanyId();
+		CD.CompanyAddress();
+	}
+
+}
